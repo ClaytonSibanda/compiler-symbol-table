2ce024c3aafb6b510fe40f4e7a6043485add8076
Clayton <sibandaclayton0@gmail.com>
2019-09-19 17:55:18 +0200

    done

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 
+import sys
+import copy
+# print(sys.argv)
 
-
-
-def handle_define(s_list,data,stack):
+def handle_define(s_list,data,stack,output):
     stack[len(stack)-1][s_list[1]] = s_list[2]
-    print(stack)
+    output.append(" ".join(s_list))
+    # print(stack)
 
-def handle_use(s_list,stack,output):
-    pass
+def handle_use(key,stack,output):
+    if len(stack)>0:
+        scope = stack[len(stack)-1]
+        if(key in scope):
+            output.append('use '+key+ ' = '+scope[key])
+        else:
+            output.append('use '+key+ ' = undefined')
+    else:
+        output.append('use '+key+ ' = undefined')
+ 
 
 def handle_endscope(stack,output):
     stack.pop()
+    output.append('endscope')
 
 def handle_beginscope(stack,output):
-    stack.append({})
+    stack.append(copy.deepcopy(stack[len(stack)-1]) if len(stack)>0 else {})
     output.append('beginscope')
 
 def handle_output(output):
@@ -25,33 +36,35 @@ def handle_output(output):
 def read_data(filename):
     with open(filename,'r') as file:
         data= [s.strip() for s in file.readlines()]
-    print(data)
+    # print(data)
     return data
 
 def construct_table(stack,output,data):
     for s in data:
         s_list = s.split()
-        #print(s_list)
+        if len(s_list)==0:
+            continue
+        # print(s_list)
         if s_list[0]=='define':
-            print('define identified')
-            handle_define(s_list,data,stack)
+            # print('define identified')
+            handle_define(s_list,data,stack,output)
                 
         elif s_list[0]=='use':
-            print('use identified')
-            handle_use(s_list,stack,output)
+            # print('use identified')
+            handle_use(s_list[1],stack,output)
         elif s=='beginscope':
-            print('begin scope')
+            # print('begin scope')
             handle_beginscope(stack,output)
           
         elif s=='endscope':
-            print('end scope')
+            # print('end scope')
             handle_endscope(stack,output)
 
 def main():
   
     stack =[]
     output = []
-    filename = input()
+    filename = sys.argv[1]
     data = read_data(filename)
     construct_table(stack,output,data)
     
